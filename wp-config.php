fbce3a8178edb989c2a8de2348072b0d8ac41730
Boris Galac <boris.galac@gmail.com>
2022-12-29 22:36:38 +0100

    update

--- wp-config.php
+++ wp-config.php
@@ -78,8 +78,6 @@ define( 'WP_DEBUG', false );
 define('FORCE_SSL_ADMIN', true);
 /* Add any custom values between this line and the "stop editing" line. */
 
-
-
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
