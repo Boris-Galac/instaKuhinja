6a415161238a1eb1ed45fc459b137b5dfd691a95
Boris Galac <boris.galac@gmail.com>
2022-12-29 13:11:51 +0100

    update

--- wp-content/themes/insta_kuhinja/header.php
+++ wp-content/themes/insta_kuhinja/header.php
@@ -5,6 +5,13 @@
     <meta charset="<?php bloginfo('charset') ?>">
     <meta name="viewport" content="width=device-width, inital-scale=1">
     <meta name="google-site-verification" content="3DKrsbCUcm8dm9sLYAJMZ4tNRso7aVi2clbVBoalNGM" />
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KTLS9W8');</script>
+    <!-- End Google Tag Manager -->
     <?php wp_head(); ?>
 </head>
 
